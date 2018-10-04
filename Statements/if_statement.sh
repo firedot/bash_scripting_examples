569deee7aec3e5784450c259d540be1002d93e0e
Alexander Todorov <alext@Alexanders-MacBook-Pro.local>
2018-10-04 11:41:49 +0300

    Added comment to if_statement.sh

--- Statements/if_statement.sh
+++ Statements/if_statement.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+
+#Reading user input and storring it into the variable "number"
+
 read -p 'Please enter a number between 0 and 10: ' number
 
-#echo $number
 
 if [ ${number} -gt 10 ]; then
 echo 'Your number is too big'
